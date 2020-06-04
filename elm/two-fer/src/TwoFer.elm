68a09b3109026799d422f614de2da6ff89e9657a
Martin Svalin <martin@lite.nu>
2020-06-04 11:07:53 +0200

    Elm two-fer feedback

--- elm/two-fer/src/TwoFer.elm
+++ elm/two-fer/src/TwoFer.elm
@@ -3,14 +3,6 @@ module TwoFer exposing (twoFer)
 
 twoFer : Maybe String -> String
 twoFer name =
-    "One for " ++ subject name ++ ", one for me."
-
-
-subject : Maybe String -> String
-subject name =
-    case name of
-        Nothing ->
-            "you"
-
-        Just name_ ->
-            name_
+    "One for "
+        ++ Maybe.withDefault "you" name
+        ++ ", one for me."
