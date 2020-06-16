b419fd31761ea68c3393a5cc4d1ddc6493a8ba2c
Martin Svalin <martin@lite.nu>
2020-06-16 23:27:31 +0200

    Elm space-age doc comment

--- elm/space-age/src/SpaceAge.elm
+++ elm/space-age/src/SpaceAge.elm
@@ -17,19 +17,18 @@ ageOn planet seconds =
     seconds / orbitalPeriodInSeconds planet
 
 
+{-|
 
-{-
-   - Mercury: orbital period 0.2408467 Earth years
-   - Venus: orbital period 0.61519726 Earth years
-   - Earth: orbital period 1.0 Earth years, 365.25 Earth days, or 31557600 seconds
-   - Mars: orbital period 1.8808158 Earth years
-   - Jupiter: orbital period 11.862615 Earth years
-   - Saturn: orbital period 29.447498 Earth years
-   - Uranus: orbital period 84.016846 Earth years
-   - Neptune: orbital period 164.79132 Earth years
+  - Mercury: orbital period 0.2408467 Earth years
+  - Venus: orbital period 0.61519726 Earth years
+  - Earth: orbital period 1.0 Earth years, 365.25 Earth days, or 31557600 seconds
+  - Mars: orbital period 1.8808158 Earth years
+  - Jupiter: orbital period 11.862615 Earth years
+  - Saturn: orbital period 29.447498 Earth years
+  - Uranus: orbital period 84.016846 Earth years
+  - Neptune: orbital period 164.79132 Earth years
+
 -}
-
-
 orbitalPeriodInSeconds : Planet -> Float
 orbitalPeriodInSeconds planet =
     case planet of
