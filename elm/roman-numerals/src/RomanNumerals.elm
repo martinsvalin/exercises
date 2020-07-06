8f6dab261f44045922bc19d7436f60da58c8207a
Martin Svalin <martin@lite.nu>
2020-07-07 00:21:48 +0200

    Elm roman-numerals with simple ifs and recursion

--- elm/roman-numerals/src/RomanNumerals.elm
+++ elm/roman-numerals/src/RomanNumerals.elm
@@ -2,31 +2,45 @@ module RomanNumerals exposing (toRoman)
 
 
 toRoman : Int -> String
-toRoman number =
-    List.foldr decimalToRoman ( number, "" ) numerals
-        |> Tuple.second
+toRoman decimal =
+    if decimal >= 1000 then
+        "M" ++ toRoman (decimal - 1000)
 
+    else if decimal >= 900 then
+        "CM" ++ toRoman (decimal - 900)
 
-decimalToRoman : ( Int, String ) -> ( Int, String ) -> ( Int, String )
-decimalToRoman ( divisor, symbol ) ( numerator, roman ) =
-    ( modBy divisor numerator
-    , roman ++ String.repeat (numerator // divisor) symbol
-    )
+    else if decimal >= 500 then
+        "D" ++ toRoman (decimal - 500)
 
+    else if decimal >= 400 then
+        "CD" ++ toRoman (decimal - 400)
 
-numerals : List ( Int, String )
-numerals =
-    [ ( 1, "I" )
-    , ( 4, "IV" )
-    , ( 5, "V" )
-    , ( 9, "IX" )
-    , ( 10, "X" )
-    , ( 40, "XL" )
-    , ( 50, "L" )
-    , ( 90, "XC" )
-    , ( 100, "C" )
-    , ( 400, "CD" )
-    , ( 500, "D" )
-    , ( 900, "CM" )
-    , ( 1000, "M" )
-    ]
+    else if decimal >= 100 then
+        "C" ++ toRoman (decimal - 100)
+
+    else if decimal >= 90 then
+        "XC" ++ toRoman (decimal - 90)
+
+    else if decimal >= 50 then
+        "L" ++ toRoman (decimal - 50)
+
+    else if decimal >= 40 then
+        "XL" ++ toRoman (decimal - 40)
+
+    else if decimal >= 10 then
+        "X" ++ toRoman (decimal - 10)
+
+    else if decimal >= 9 then
+        "IX" ++ toRoman (decimal - 9)
+
+    else if decimal >= 5 then
+        "V" ++ toRoman (decimal - 5)
+
+    else if decimal >= 4 then
+        "IV" ++ toRoman (decimal - 4)
+
+    else if decimal > 0 then
+        "I" ++ toRoman (decimal - 1)
+
+    else
+        ""
