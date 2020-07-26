4b1d20c2640e1b5e704088dda853267c5d34bf48
Martin Svalin <martin@lite.nu>
2020-07-26 23:17:00 +0200

    Kotlin raindrops with String extension

--- kotlin/raindrops/src/main/kotlin/Raindrops.kt
+++ kotlin/raindrops/src/main/kotlin/Raindrops.kt
@@ -1,17 +1,15 @@
 object Raindrops {
 
-    fun convert(n: Int): String {
-        val sound = pling(n) + plang(n) + plong(n)
-        if (sound.isEmpty())
-            return n.toString()
-        else
-            return sound
-    }
+    fun convert(n: Int): String =
+        (pling(n) + plang(n) + plong(n)).default(n.toString())
 
     private fun pling(n: Int): String = dropSound(n, 3, "Pling")
     private fun plang(n: Int): String = dropSound(n, 5, "Plang")
     private fun plong(n: Int): String = dropSound(n, 7, "Plong")
 
-    private fun dropSound(n: Int, denominator: Int, sound: String): String =
-        if (n % denominator == 0) sound else ""
+    private fun dropSound(n: Int, factor: Int, sound: String): String =
+        if (n % factor == 0) sound else ""
+
+    private fun String.default(default: String) =
+        if (this.isEmpty()) default else this
 }
