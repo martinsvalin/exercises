32aa784879b3dda1488b6439f1e3e36647608061
Martin Svalin <martin@lite.nu>
2020-08-01 22:02:33 +0200

    Kotlin hamming for-loop

--- kotlin/hamming/src/main/kotlin/Hamming.kt
+++ kotlin/hamming/src/main/kotlin/Hamming.kt
@@ -2,14 +2,15 @@ import java.lang.IllegalArgumentException
 
 object Hamming {
 
-    fun compute(leftStrand: String, rightStrand: String): Int =
-        if (leftStrand.length == rightStrand.length)
-            leftStrand.foldIndexed(0) { i, distance, char ->
-                if (char == rightStrand[i])
-                    distance
-                else
-                    distance.inc()
+    fun compute(leftStrand: String, rightStrand: String): Int {
+        if (leftStrand.length == rightStrand.length) {
+            var distance = 0
+            for (i in leftStrand.indices) {
+                if (leftStrand[i] != rightStrand[i]) distance++
             }
-        else
+            return distance
+        } else {
             throw IllegalArgumentException("left and right strands must be of equal length")
+        }
+    }
 }
