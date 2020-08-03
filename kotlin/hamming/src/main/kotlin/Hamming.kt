35291ddaf0b1181295fafd550175143aacefb6e9
Martin Svalin <martin@lite.nu>
2020-08-03 21:53:46 +0200

    Kotlin hamming with require and zip->count

--- kotlin/hamming/src/main/kotlin/Hamming.kt
+++ kotlin/hamming/src/main/kotlin/Hamming.kt
@@ -1,16 +1,11 @@
-import java.lang.IllegalArgumentException
-
 object Hamming {
 
     fun compute(leftStrand: String, rightStrand: String): Int {
-        if (leftStrand.length == rightStrand.length) {
-            var distance = 0
-            for (i in leftStrand.indices) {
-                if (leftStrand[i] != rightStrand[i]) distance++
-            }
-            return distance
-        } else {
-            throw IllegalArgumentException("left and right strands must be of equal length")
+        require(leftStrand.length == rightStrand.length) {
+            "left and right strands must be of equal length"
         }
+        return leftStrand
+            .zip(rightStrand)
+            .count { it.first != it.second }
     }
 }
