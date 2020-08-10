aa37aa47353deacf29805952e417c1228f2fe824
Martin Svalin <martin@lite.nu>
2020-08-10 23:35:55 +0200

    Kotlin transpose with trim/replace for placeholder \u0000

--- exercism-latest/kotlin/transpose/src/main/kotlin/Transpose.kt
+++ exercism-latest/kotlin/transpose/src/main/kotlin/Transpose.kt
@@ -1,12 +1,13 @@
 object Transpose {
+    const val PLACEHOLDER = '\u0000'
 
     fun transpose(strings: List<String>): List<String> =
         0.until(longest(strings)).map { index ->
             strings
-                .map { it.getOrNull(index) }
-                .dropLastWhile { it == null } // trim on the right
-                .map { it ?: ' ' } // pad with space on the left
+                .map { it.getOrElse(index) { PLACEHOLDER } }
                 .joinToString("")
+                .trimEnd(PLACEHOLDER)
+                .replace(PLACEHOLDER, ' ')
         }
 
     private fun longest(strings: List<String>): Int =
